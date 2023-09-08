b41242c452ee45ea4f2cc4dbf2bf882104878a70
NathanAzvdo <nathan.azevedo28@gmail.com>
2023-09-08 17:40:31 -0300

    first commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControledeContatos.Controllers
+{
+    public class ContatoController : Controller
+    { 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult criar()
+        {
+            return View();
+        }
+        public IActionResult editar()
+        {
+            return View();
+        }
+        public IActionResult apagar()
+        {
+            return View();
+        }
+    }
+}
